bf3d0893600efffc9b484dd512c0da28ca06a808
Bilal Itani <bilalitani1@gmail.com>
2017-08-13 10:07:55 -0400

    Complete project

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/* Copyright (C) Xenfinity LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bilal Itani <bilalitani1@gmail.com>, June 2017
+ */
+
+public class MainMenuController : MonoBehaviour {
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
